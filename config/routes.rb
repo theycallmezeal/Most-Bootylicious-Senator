dee0af72dd7b017c9544df7a2794c0b08c7c8d72
Zeal An <zealan@gmail.com>
2015-01-17 16:51:16 -0500

    Stylez

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get '/' => "senators#vote"
-  get '/new' => "senators#new"
+  get '/vote' => "senators#vote"
   get '/stats' => "senators#display"
   get '/about' => "senators#about"
-  get '/create' => "senators#create"
-  post '/create' => "senators#create"
 end
